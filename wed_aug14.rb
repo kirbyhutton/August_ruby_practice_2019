68ec098b40b7ab917503f558fe52711a61f97036
Kirby <khutton@periscopedata.com>
2019-08-19 11:10:20 -0700

    completed nested iterators in ruby

--- wed_aug14.rb
+++ wed_aug14.rb
@@ -1,9 +1,55 @@
 # each iterator#
 
-arr = [23, 52, 11, 3454, 30908]
+# arr = [23, 52, 11, 3454, 30908]
 
 # arr.each do |i|
 # 	p i
+# end
 
-arr.each { |i| p i }
+# arr.each { |i| p i }
+
+
+#these two are the exact same, one line or a few
+
+####using the for loop in Ruby####
+
+#these are not used as much, each is way more common#
+
+# for i in 0..42
+# 	p i
+# end
+	#0..42 is creating a collection#
+
+#how to build a nested iterator#
+
+
+teams = {
+	"Houston Astros" => {
+		"first base" => "Chris Carter",
+		"second base" => "Jose Altuve",
+		"shortstop" => "Carlos Correa"
+	},
+	"Texas Rangers" => {
+		"first base" => "Prince Fielder",
+		"second base" => "R. Odor",
+		"shortstop" => "Elvis Andrus"
+	}
+
+}
+
+# teams.each do |team|
+# 	p team
+# end
+
+teams.each do |team, players|
+	puts team
+	players.each do |position, player|
+		p "#{player} starts at #{position}"
+	end
 end
+
+
+
+
+
+
