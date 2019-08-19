4b27009ed7bb9dd568101fb4ad0bee2529988ab3
Kirby <khutton@periscopedata.com>
2019-08-19 11:34:18 -0700

    using the select method

--- wed_aug14.rb
+++ wed_aug14.rb
@@ -11,7 +11,7 @@
 
 #these two are the exact same, one line or a few
 
-####using the for loop in Ruby####
+####using the for loop in Ruby#########################################
 
 #these are not used as much, each is way more common#
 
@@ -20,33 +20,103 @@
 # end
 	#0..42 is creating a collection#
 
-#how to build a nested iterator#
+#how to build a nested iterator####################################
 
 
-teams = {
-	"Houston Astros" => {
-		"first base" => "Chris Carter",
-		"second base" => "Jose Altuve",
-		"shortstop" => "Carlos Correa"
-	},
-	"Texas Rangers" => {
-		"first base" => "Prince Fielder",
-		"second base" => "R. Odor",
-		"shortstop" => "Elvis Andrus"
-	}
+# teams = {
+# 	"Houston Astros" => {
+# 		"first base" => "Chris Carter",
+# 		"second base" => "Jose Altuve",
+# 		"shortstop" => "Carlos Correa"
+# 	},
+# 	"Texas Rangers" => {
+# 		"first base" => "Prince Fielder",
+# 		"second base" => "R. Odor",
+# 		"shortstop" => "Elvis Andrus"
+# 	}
 
-}
+# }
 
 # teams.each do |team|
 # 	p team
 # end
 
-teams.each do |team, players|
-	puts team
-	players.each do |position, player|
-		p "#{player} starts at #{position}"
-	end
-end
+# teams.each do |team, players|
+# 	puts team
+# 	players.each do |position, player|
+# 		p "#{player} starts at #{position}"
+# 	end
+# end
+
+#using the select method in Ruby#################################
+
+
+#given an array of integers
+#Only grab the even integers
+
+# while x is <100
+# 	if i % 2 == 0 
+# 		puts i
+
+#select in Ruby does this!
+
+# (1..10).to_a.select do |x|
+# 	 x.even?
+# end
+
+# (1..10).to_a.select {|x| x.even? }
+
+# (1..10).to_a.select(&:even?)
+
+
+#given an array of strings
+#return only the words that are over 5 letters
+
+
+%w(The quick brown fox jumped over the lazy dog)
+
+arr = %w(The quick brown fox jumped over the lazy dog)
+
+arr.select { |x| x.length >5 }
+
+
+#given an array of strings
+#return all of the vowels
+
+
+%w(a b c d e f g).select { |v| v =~ /[aeiou]/ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
